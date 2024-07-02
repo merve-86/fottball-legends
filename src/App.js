7fa549b2f0819004bae5a883f2cb298416d8a611
merve <merveaydin86@hotmail.com>
2024-07-03 01:54:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container"
+import Header from "./components/header/Header"
+import LegendContainer from "./components/legends/LegendsContainer";
+
+
+
+function App() {
+  return (
+    <Container>
+      <Header />
+      <LegendContainer />
+    </Container>
+
+  )
+}
+
+export default App;
